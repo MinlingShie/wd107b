2dcc1da1321d77bfb2447a3ed1958676e7077874
MinlingShie <australia851009@gmail.com>
2019-05-24 10:57:35 +0800

    update

--- exercise/04-nodejs/HW/hw4a-04.js
+++ exercise/04-nodejs/HW/hw4a-04.js
@@ -2,17 +2,16 @@
 /* 請請算出某陣列的平均值？
 範例： mean([1,2,3,4,5]) => 3 */
 
-/*
+ 
 function average()
 {
     var sum=0;
     var a=[1,2,3,4,5];
-    for(o of a)
-    {
-        sum+=o;
+    for (i=0;i<a.length;i++){
+        sum=sum+a[i]
+        console.log('sum=',sum)
     }
-    sum/=a.length;
-    console.log(sum);
+    var average=sum/a.length
+    console.log('average=',average)
 }
 average();
-*/
